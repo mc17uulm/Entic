4a56e2e67b520ef7503400a1655fa4f4f8a12722
Kaith Menken <kaith-uwe.menken@uni-ulm.de>
2018-03-19 17:00:48 +0100

    added 16562+626+

---
+++ PlasticPeople/Assets/SettingsMenu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SettingsMenu : MonoBehaviour {
+
+    public AudioMixer audioMixer;
+
+	public void SetMusicVolume (float musicVolume)
+    {
+        audioMixer.SetFloat("MusicVolume", musicVolume);
+    }
+
+    public void SetSoundVolume (float soundVolume)
+    {
+        audioMixer.SetFloat("SoundVolume", soundVolume);
+    }
+}
