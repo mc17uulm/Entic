45026aa192b17a238fbdef564a1af55c4ec47d45
Kaith Menken <kaith-uwe.menken@uni-ulm.de>
2018-03-23 09:07:16 +0100

    progress

---
+++ PlasticPeople/Assets/Scripts/DateSystem.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DateSystem : MonoBehaviour {
+
+    private const int TIMESCALE = 45;
+
+    private DayOfWeek dayofweek;
+    private double day, second, month, year;
+
+    public Text dayInWeekText, dayText, monthText, yearText; 
+
+	// Use this for initialization
+	void Start () {
+        month = System.DateTime.Now.Month;
+        day = DateTime.Now.Day;
+        dayofweek = DateTime.Now.DayOfWeek;
+        year = DateTime.Now.Year;
+        
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+	}
+
+    void CalculateMonth()
+    {
+
+    }
+
+    void CalculateDay()
+    {
+
+    }
+
+}
---
+++ PlasticPeople/Assets/Scripts/SpeedChange.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SpeedChange : MonoBehaviour {
+
+    public Slider slider;
+    public Sprite pauseImage;
+    public Sprite playImage;
+    public Sprite fastplayImage;
+
+	public void onChange()
+    {
+        Debug.Log("triggered");
+        if(slider.value == 0)
+        {
+            Debug.Log("pause");
+            slider.handleRect.gameObject.GetComponent<Image>().sprite = pauseImage;
+        }
+        else if(slider.value == 1)
+        {
+            Debug.Log("play");
+            slider.handleRect.gameObject.GetComponent<Image>().sprite = playImage;
+        }
+        else
+        {
+            Debug.Log("gotta go fast");
+            slider.handleRect.gameObject.GetComponent<Image>().sprite = fastplayImage;
+        }
+
+    }
+
+}
