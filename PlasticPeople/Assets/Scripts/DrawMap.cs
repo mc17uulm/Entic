24f86e16c0f3b78b8951ee98fc1ba3f93ef3da1a
Kaith Menken <kaith-uwe.menken@uni-ulm.de>
2018-03-28 17:32:59 +0200

    tried scripting the images but it failed

---
+++ PlasticPeople/Assets/Scripts/DrawMap.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DrawMap : MonoBehaviour {
+
+
+    public List<Sprite> Sprites = new List<Sprite>();
+    public GameObject ParentPanel;
+    // Use this for initialization
+    void Awake () {
+        foreach (Sprite currentSprite in Sprites)
+        {
+            Debug.Log("Sprite "+currentSprite.name);
+            GameObject NewObj = new GameObject(); //Create the GameObject
+            Image NewImage = NewObj.AddComponent<Image>(); //Add the Image Component script
+            NewImage.sprite = currentSprite; //Set the Sprite of the Image Component on the new GameObject
+            //NewImage.SetNativeSize();
+            NewObj.GetComponent<RectTransform>().SetParent(ParentPanel.transform); //Assign the newly created Image GameObject as a Child of the Parent Panel.
+            NewObj.name = currentSprite.name;
+            NewObj.SetActive(true); //Activate the GameObject
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
