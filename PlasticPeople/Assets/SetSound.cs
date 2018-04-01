c1c75e7941cd1c437169127ffdbb687d4fba4df0
Kaith Menken <kaith-uwe.menken@uni-ulm.de>
2018-04-01 20:51:25 +0200

    alle hände hoc

---
+++ PlasticPeople/Assets/SetSound.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class SetSound : MonoBehaviour {
+
+    public bool isMusic;
+
+    public AudioMixer mixer;
+
+	// Use this for initialization
+	void Start () {
+        float currVolume = 0;
+        if (isMusic)
+        {
+            mixer.GetFloat("MusicVolume", out currVolume);
+            gameObject.GetComponent<Slider>().value = currVolume;
+        }
+        else
+        {
+            mixer.GetFloat("SoundVolume", out currVolume);
+            gameObject.GetComponent<Slider>().value = currVolume;
+        }
+	}
+	
+}
